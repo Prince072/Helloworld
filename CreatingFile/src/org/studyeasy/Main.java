2d5a46f4f8b9620993240432f9f290a920e69dfb
prince <prince@localhost>
2021-06-30 08:28:39 +0200

    First commit

---
+++ CreatingFile/src/org/studyeasy/Main.java
@@ -0,0 +1,16 @@
+package org.studyeasy;
+
+
+
+public class Main {
+
+	public static void main(String[] args) throws Exception {
+	
+        int x =5,y=0;
+        int r = x/y;
+        System.out.println(r);
+
+	}
+
+}
+ 
